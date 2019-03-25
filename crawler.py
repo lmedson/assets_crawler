bc0b1bdd56ebf85c668e459268201a5945824bf4
medson mendes <medson-mbp@hotmail.com>
2019-03-25 13:14:07 -0300

    fix scraped relations

--- crawler.py
+++ crawler.py
@@ -1,84 +1,69 @@
 from bs4 import BeautifulSoup
-from prettytable import PrettyTable
 import urllib.request
-import json
-from multiprocessing.dummy import Pool
+from prettytable import PrettyTable
 
 
 class Crawler():
     def __init__(self, url):
         self.url = url
-        self.list_links = []
-        self.new_links = []
+        self.list_links = [url]
         self.assets_map = []
         self.relation_links = []
-        # self.links_filtereds = []
         self.crawled = []
-        self.req = urllib.request.urlopen(url)
-        self.soup = BeautifulSoup(self.req, features='lxml')
-        # get url string links
-        for link in self.soup.find_all('a', href=True):
-            if link['href'].startswith('/'):
-                if self.url+link['href'] not in self.list_links:
-                    self.list_links.append(self.url+link['href'])
-            else:
-                pass
 
-    def filter_url(self, link):
+    def filter_url(self, link, baseUrl):
+        new_url = self.url[:-1]+link
         if link.startswith('/'):
-            return self.url+link
+            return new_url
+        elif link.startswith(baseUrl):
+            return link
+        else:
+            pass
 
-    def make_map(self):
-        for url in range(len(self.list_links)):
-            if len(self.list_links) == url:
-                for url in range(len(self.new_links)):
-                    if self.new_links[url] not in (self.crawled and self.list_links):
-                        new_req = urllib.request.urlopen(self.new_links[url])
-                        new_soup = BeautifulSoup(
-                            new_req, features='lxml')
-                        for i in new_soup.find_all('a'):
-                            link = i.get('href')
-                            filtered = self.filter_url(link)
-                            if filtered != None:
-                                self.crawled.append(self.new_links[url])
-                                links_filtereds.append(filtered)
-                                if filtered not in (self.list_links and self.new_links and self.crawled):
-                                    self.new_links.append(filtered)
-                                else:
-                                    pass
-                    self.relation_links.append(
-                        {'page': self.new_links[url], 'related_links': links_filtereds})
-            else:
-                links_filtereds = []
-                if self.list_links[url] not in self.crawled:
-                    if self.list_links[url] in self.list_links:
-                        # cria uma nova request com base na url da vez
-                        new_req = urllib.request.urlopen(self.list_links[url])
-                        new_soup = BeautifulSoup(
-                            new_req, features='lxml')
-                        # percorre todas as tags 'a' na requisição realizada
-                        for i in new_soup.find_all('a'):
-                            # pega o endereço da tag da vez
-                            link = i.get('href')
-                            # checa se é do domínio
-                            filtered = self.filter_url(link)
-                            if filtered != None:
-                                # se for válida adiciona a url da vez na lista de crawleadas
-                                self.crawled.append(self.list_links[url])
-                                # adiciona ao vetor que guarda as relações da url da vez
-                                links_filtereds.append(filtered)
-                                if filtered not in (self.list_links and self.new_links and self.crawled):
-                                    self.new_links.append(filtered)
-                                else:
-                                    pass
+    def get_urls_map(self):
+        urls_to_crawl = self.list_links.copy()
+        crawled_list = []
+        while len(urls_to_crawl) != 0:
+            current_url = urls_to_crawl[0]
+            if current_url not in crawled_list:
+                links_relateds = []
+                try:
+                    req = urllib.request.urlopen(current_url)
+                    soup = BeautifulSoup(req, features='lxml')
+                except:
+                    pass
+                # add current url to crawleds list
+                crawled_list.append(current_url)
+                for i in soup.find_all("a"):
+                    # get found url
+                    found_url = i["href"]
+                    # check if found url belongs to base url
+                    filtered_url = self.filter_url(found_url, self.url)
+                    if filtered_url != None and filtered_url not in links_relateds:
+                        # print(filtered_url)
+                        """
+                        if filtered belongs to base url, and not yet in current
+                        crawled url, push to array with relashionships
+                        """
+                        links_relateds.append(filtered_url)
+                        # if the found url yet not crawled, push to array to craw
+                        if filtered_url not in crawled_list and filtered_url not in urls_to_crawl:
+                            # add found url inside current_url to crawl list
+                            urls_to_crawl.append(filtered_url)
+                        else:
+                            pass
                 self.relation_links.append(
-                    {'page': self.list_links[url], 'related_links': links_filtereds})
+                    {'page': current_url, 'related_links': links_relateds})
+                urls_to_crawl.pop(urls_to_crawl.index(current_url))
+            else:
+                self.crawled = crawled_list
+                return print("Crawling finished")
 
-    def get_map(self):
-        self.make_map()
+    def run(self):
+        self.get_urls_map()
         return self.relation_links
 
-    def crawl(self):
+    def crawl_assets(self):
         # get urls
         for page in self.list_links:
             new_req = urllib.request.urlopen(page)
@@ -87,12 +72,9 @@ class Crawler():
             for index in new_soup.find_all('img'):
                 alt = index.get('alt')
                 src = index.get('src')
-                if src.startswith('/'):
-                    self.assets_map.append(
-                        {'page': page,  'images': {'name_image': alt, 'link_image': self.url+src}, 'css': [], 'js': []})
-                else:
-                    self.assets_map.append(
-                        {'page': page, 'images': {'name_image': alt, 'link_image': src}, 'css': [], 'js': []})
+                filtered_url = self.filter_url(src, self.url)
+                self.assets_map.append(
+                    {'page': page,  'images': {'name_image': alt, 'link_image': filtered_url}, 'css': [], 'js': []})
         # get css
         for key in range(len(self.assets_map)):
             for index_css in new_soup.find_all('link'):
@@ -121,9 +103,8 @@ class Crawler():
         return self.assets_map
 
     def storage_data(self, filename1, filename2):
-
         save_file = open(filename1, "w+")
-        data_to_print = self.crawl()
+        data_to_print = self.crawl_assets()
         table = PrettyTable(
             ["page", "image_name", "image_link", "css", "js"])
         for element in data_to_print:
@@ -134,7 +115,7 @@ class Crawler():
         save_file.close()
 
         save_file_map = open(filename2, "w+")
-        data_to_print_map = self.get_map()
+        data_to_print_map = self.run()
         table_map = PrettyTable(
             ["page", "relations"])
         for element in data_to_print_map:
--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 from crawler import Crawler
 import time
-from threading import Thread
+
+start_time = time.time()
 # # example
 # declaring url to get using the crawler
 url_to_scrape = 'https://elixir-lang.org/'
 new_crawler = Crawler(url_to_scrape)
 
+new_crawler.run()
 # using the method crawl to get and format data
-new_crawler.storage_data(
-    'my_scraped_data.txt', 'my_scraped_data1.txt',)
+new_crawler.storage_data("my_scraped_assets.txt", "my_scraped_relations.txt")
+
+exec_time = time.time() - start_time
+print("Execution time: ", exec_time)
